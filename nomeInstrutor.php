5696a66946fe3d737a64b99b7155053d65b339e1
Bárbara Letícia Soares <barbaracavalcanti665@gmail.com>
2025-05-26 09:45:19 -0300

    Add files via upload

---
+++ nomeInstrutor.php
@@ -0,0 +1,12 @@
+<?php
+
+$instrutor = "Rafael";
+
+$num1 = 5; 
+$num2 = 9;
+$num3 = 6;
+$num4 = 12;
+$num5 = 23;
+$multiplicacao = $num1 * $num2 * $num3 * $num4 * $num5;
+
+echo "O nome do instrutor é: " .$instrutor. " e o resultado da multiplicação é: ".$multiplicacao;
