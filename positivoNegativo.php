1f64941388bee962acf0783e04b6aebe9af1c787
Bárbara Letícia Soares <barbaracavalcanti665@gmail.com>
2025-05-29 09:08:58 -0300

    Add files via upload

---
+++ positivoNegativo.php
@@ -0,0 +1,16 @@
+<?php
+
+$num = 5;
+
+if($num > 0)
+{
+    echo "O número " .$num. " é positivo!";
+}
+else if ($num < 0)
+{
+    echo "O número " .$num. " é negativo!";
+}
+else
+{
+    echo "O número é zero!";
+}
