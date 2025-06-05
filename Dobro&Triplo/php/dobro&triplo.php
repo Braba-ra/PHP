c44a5a34a042e88e9f3bf479ae851ef5d6c1b2ae
Bárbara Letícia Soares <barbaracavalcanti665@gmail.com>
2025-06-05 11:03:19 -0300

    Add files via upload

---
+++ Dobro&Triplo/php/dobro&triplo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dobro & Triplo</title>
+</head>
+<body>
+    
+<h1>Resultado</h1>
+
+<?php
+
+$num = 10;
+$dobro = 10 * 2;
+$triplo = 10 * 3;
+
+echo "O dobro do número ".$num." é : ".$dobro. "! <br>";
+echo "O triplo do número ".$num." é : ".$triplo. "!";
+
+?>
+
+</body>
+</html>
